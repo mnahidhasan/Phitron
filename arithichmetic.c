0befbcf4e0ed6337125e6df74bced9062b900909
Nahid Hasan <mhasannahid15@gmail.com>
2026-04-13 13:51:33 +0600

    first commit

---
+++ arithichmetic.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  float a=16;
+  int b=2;
+//   int sum = a+b;
+//   printf(" Sumation = %d\n",sum);
+//   int sub =a-b;
+//   printf(" Subtrucktion = %d\n",sub);
+//   int mul= a*b;
+//   printf("Multiplication =%d\n",mul);
+
+
+  float div=a/b;
+  printf("Division =%.1f",div);
+
+    return 0;
+}
