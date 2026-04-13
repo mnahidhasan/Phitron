a5613e48b88ccf5c14d1e2fe093f88d4c645ca0c
Md . Nahid Hasan <mhasannahid15@gmail.com>
2026-04-13 14:43:17 +0600

    Add files via upload

---
+++ if_else_ladder.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+  int tk;
+  scanf("%d",& tk);
+  if(tk>=100)
+  {
+    printf("Burger Khabo\n",tk);
+  }
+ else if(tk>=50)
+ {
+    printf("Fuchka khabo\n",tk);
+ }
+ else if(tk>=20)
+ {
+    printf("chips khabo \n",tk);
+ }
+
+else 
+
+{
+    printf("kicui khabo na",tk);
+}
+
+
+
+
+
+    return 0;
+}
